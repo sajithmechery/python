37ef940854ab623d5cd92b5623bf9b696f71da0e
sajithmechery <47314393+sajithmechery@users.noreply.github.com>
2019-02-05 14:50:40 +0530

    Update vowelornot.py

--- vowelornot.py
+++ vowelornot.py
@@ -1,7 +1,6 @@
-ch=('a','e','i','o','u','A','E','I','O','U'):
-s=input("Enter the character:")
+ch=('a','e','i','o','u','A','E','I','O','U')
+s = input("Enter a character: ")
 if(s in ch):
-  print("is a vowel")
-  else:
-    print("is a consonant")
-   
+    print("is a Vowel")
+else:
+    print("is a Consonant")
